e720addeddc5c18871f6524c4998f8f0ff4a090a
Jürgen Kober <mindeleven@posteo.de>
2020-03-16 18:43:30 +0100

    Fetching posts from JSONPlaceholder via axios.

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 
 class ResourceList extends React.Component {
+  state = { resources: [] };
+
+  async componentDidMount() {
+    const response = await axios.get(`http://jsonplaceholder.typicode.com/${this.props.resource}`);
+    this.setState({ resources: response.data });
+  }
+
   render() {
     return (
-      <div>{this.props.resource}</div>
+      <div>{this.state.resources.length}</div>
     );
   }
 }

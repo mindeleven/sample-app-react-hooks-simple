e844c4ba5c6bb37339802de949dced5b774c7e6c
Jürgen Kober <mindeleven@posteo.de>
2020-03-16 15:11:32 +0100

    Added some basic JSX.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = { resource: 'posts' }
   render() {
     return (
-      <div>App</div>
+      <div>
+        <div>
+          <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
+          <button onClick={() => this.setState({resource: 'todos'})}>Todos</button>
+        </div>
+        {console.log(this.state.resource)}
+      </div>
     );
   }
 }

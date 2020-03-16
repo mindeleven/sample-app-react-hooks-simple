f24f927542f28008b6f08c5926b474d15d2bb717
Jürgen Kober <mindeleven@posteo.de>
2020-03-16 17:51:54 +0100

    Created ResourceList component.

---
+++ src/components/ResourceList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ResourceList extends React.Component {
+  render() {
+    return (
+      <div>ResourceList</div>
+    );
+  }
+}
+
+export default ResourceList;
